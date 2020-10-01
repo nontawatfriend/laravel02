602510c543667b4f73d0868d822078c803d505b0
nontawat <friend_25411998@hotmail.com>
2020-10-01 23:29:45 +0700

    test2

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -26,6 +26,7 @@
 
 <body id="page-top">
 {{-- tests --}}
+{{-- tests2 --}}
   <!-- ======= Header/ Navbar ======= -->
   <nav class="navbar navbar-b navbar-trans navbar-expand-md fixed-top" id="mainNav">
     <div class="container">
